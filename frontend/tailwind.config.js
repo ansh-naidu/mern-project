a9ba4ab38ab95a36b92f18f18d1f861436754316
ansh-naidu <anshdnaidu@gmail.com>
2024-05-08 16:04:50 +0530

    Front end commit

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,19 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        "dark-purple": "#081A51",
-        "light-white": "rgba(255,255,255,0.17)",
-      },
-    },
-  },
-  plugins: [
-    require('@tailwindcss/forms'),
-  ],
-}
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        
+        colors: {
+            'white': '#FFFFFF',
+            'black': '#242424',
+            'grey': '#F3F3F3',
+            'dark-grey': '#6B6B6B',
+            'red': '#FF4E4E',
+            'transparent': 'transparent',
+            'twitter': '#1DA1F2',
+            'purple': '#8B46FF'
+        },
 
+        fontSize: {
+            'sm': '12px',
+            'base': '14px',
+            'xl': '16px',
+            '2xl': '20px',
+            '3xl': '28px',
+            '4xl': '38px',
+            '5xl': '50px',
+        },
+
+        extend: {
+            fontFamily: {
+              inter: ["'Inter'", "sans-serif"],
+              gelasio: ["'Gelasio'", "serif"]
+            },
+        },
+
+    },
+    plugins: [],
+};
