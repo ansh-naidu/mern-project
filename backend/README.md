763280767351752c7c9b58d929a9fdb44fb11831
ansh-naidu <anshdnaidu@gmail.com>
2024-05-08 16:09:33 +0530

    final commit

--- backend/README.md
+++
@@ -1,7 +0,0 @@
-# backend
-
-```bash
-npm init
-npm i dotenv express mongoose mongoose-unique-validator jsonwebtoken multer uuid express-validator bcryptjs
-npm i -D nodemon
-```
--- frontend/src/home/pages/Home.jsx
+++
@@ -1,7 +0,0 @@
-const Home = () => {
-    return (
-        <div className="w-full flex place-items-center justify-center p-6 text-lg">Home!</div>
-    );
-};
-
-export default Home;
